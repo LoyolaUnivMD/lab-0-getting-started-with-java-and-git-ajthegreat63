d829d6d5f184e5a11469a17bcc818170742d7e52
amari <ajpiper@loyola.edu>
2024-01-21 17:29:08 -0500

    Finished HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,25 @@
-//HelloWorld.java
+// Programmers: Amari Piper
+// Course:  CS 212, Mr. Nweke
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits: N/A
 
 class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
-        
+
+        System.out.println("Here are my initials:");
+        System.out.println("_________________________________");
+        System.out.println("_________________________________");
+        System.out.println("   _____ __________ ");
+        System.out.println("  /  _  \\\\______   \\");
+        System.out.println(" /  /_\\  \\|     ___/");
+        System.out.println("/    |    \\    |    ");
+        System.out.println("\\____|__  /____|    ");
+        System.out.println("        \\/          ");
     }
 }
